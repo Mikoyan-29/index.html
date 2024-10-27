08b34440908ee96f3a5e3d21d7fc1e37f3bf3ae5
Mikoyan-29 <mikoyangurevick@gmail.com>
2024-10-27 11:15:42 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,13 @@
+//Alerta\\
+alert('Agora você vai ver o projeto desenvolvido por Miguel Aluno da One Bit Code! Aproveite 😁')
+
+//Lista de funcionarios\\
+document.getElementById('orange2').style.backgroundColor= "#fc745c";
+document.getElementById('orange1').style.backgroundColor= "#fc745c";
+document.getElementById('orange3').style.backgroundColor= "fc745c";
+document.getElementById('orange4').style.backgroundColor= "fc745c";
+document.getElementById('orange5').style.backgroundColor= "#fc745c";
+
+//Cabeçalho\\
+document.getElementById('laranja').style.backgroundColor= "#fc745c";
+document.getElementById('laranja').style.color= "#ffff";
